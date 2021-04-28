73b940beb4db8d0c144c5c646ca194a3725e3641
Illya-pas <illya.pasi4nik08@gmail.com>
2021-04-28 15:03:31 +0300

    change package.json settings for build

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,39 +1,74 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CustomButton from "../components/menuItems/CustomButton";
 import ItemSlider from "../components/itemCards/ItemSlider";
+import { colors } from "../theme/colors";
+import { addToCart } from "../redux/actions";
+import Divider from "../components/Divider";
 
 const useStyles = makeStyles({
 	root: {
 		display: "flex",
-		// "& h1": {
-		// 	width: "fit-content",
-		// 	marginLeft: 30,
-		// 	marginTop: 50,
-		// 	fontFamily: "Open Sans Condensed",
-		// 	fontSize: 54,
-		// },
+		borderBottom: `1px solid ${colors.secondary}`,
 	},
 	info: {
 		flexGrow: 1,
+		"& h3": {
+			fontSize: 25,
+			fontWeight: 600,
+			padding: " 0 0 30px 0",
+			lineHeight: "33.25px",
+		},
 	},
 	itemInfo: {
-		"& h3": {
-			padding: 30,
-			fontSize: 25,
-			lineHeight: "33.25px",
-			fontWeight: 600,
+		padding: 40,
+		// "& h3": {
+		// },
+	},
+	itemId: {
+		fontWeight: 300,
+		fontSize: 17,
+		width: "100%",
+		maxWidth: 340,
+		display: "flex",
+		justifyContent: "space-between",
+		"& p": {
+			color: "#7D7E7D",
 		},
+	},
+	itemPrice: {
+		display: "flex",
+		marginTop: 15,
+		alignItems: "center",
+		width: "100%",
+		maxWidth: 500,
+		justifyContent: "space-between",
+		"@media (max-width: 1300px)": {
+			flexDirection: "column",
+			alignItems: "flex-start",
+		},
+		"& h2": {
+			minWidth: 204,
+			fontSize: 30,
+			fontWeight: 600,
+			marginTop: 15,
+		},
+	},
+	characteristics: {
+		display: "flex",
+		padding: 40,
 	},
 });
 
 export default function Article() {
+	const dispatch = useDispatch();
 	const classes = useStyles();
 	let splitedHref = window.location.href.split("/");
 	let currentItem = splitedHref[splitedHref.length - 1];
 
-	const item = useSelector((state) => state.cards.cards.tobacco[1]);
+	const item = useSelector((state) => state.cards.cards.tobacco[0]);
+	const cart = useSelector((state) => state.cart.cart);
 
 	const [characteristics, setCharacteristics] = useState([
 		{
@@ -62,6 +97,15 @@ export default function Article() {
 		},
 	]);
 
+	let buttonStyles = {
+		fontWeight: 500,
+		fontSize: 28,
+		borderRadius: 3,
+		padding: "10px 30px",
+		width: "fit-content",
+		marginTop: 15,
+	};
+
 	return (
 		<div className={classes.root}>
 			<ItemSlider item={item} />
@@ -70,13 +114,43 @@ export default function Article() {
 					<h3>
 						{item.type} <br /> {item.name}
 					</h3>
-					{item.available ? (
-						<span style={{ color: "green" }}>В наявності</span>
-					) : (
-						<span style={{ color: "red" }}>Немає в наявності</span>
-					)}
+					<div className={classes.itemId}>
+						{item.available ? (
+							<span style={{ color: "green" }}>В наявності</span>
+						) : (
+							<span style={{ color: "red" }}>Немає в наявності</span>
+						)}
+						<p>Артикул: {item.id}</p>
+					</div>
+					<div className={classes.itemPrice}>
+						<div>
+							<h2>{item.price} грн/шт</h2>
+							<h2>{item.price * 10} грн/блок</h2>
+						</div>
+						<CustomButton
+							primary={colors.secondary}
+							secondary={colors.primary}
+							text="КУПИТИ"
+							action={() => dispatch(addToCart(item, cart))}
+							styles={buttonStyles}
+						/>
+					</div>
 				</div>
-				<div></div>
+
+				<Divider />
+
+				<div className={classes.characteristics}>
+					<h3>Характеристики</h3>
+					<div className={classes.characteristicsInfo}>
+						<div className={classes.characteristicsInfo.left}>
+							{characteristics.map((characteristic) => {})}
+						</div>
+						<div className={classes.characteristicsInfo.right}></div>
+					</div>
+				</div>
+
+				<Divider />
+
 				<div></div>
 			</div>
 		</div>
