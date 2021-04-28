8aecb8fe2238eccfe086c8d33f8d6968033c8589
Illya-pas <illya.pasi4nik08@gmail.com>
2021-04-28 15:05:27 +0300

    change package.json settings for build

--- src/index.js
+++ src/index.js
@@ -8,13 +8,7 @@ import thunk from "redux-thunk";
 import { rootReducer } from "./redux/reducers/rootReducer";
 import App from "./App";
 
-const store = createStore(
-	rootReducer,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-);
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 const app = (
 	<Provider store={store}>
